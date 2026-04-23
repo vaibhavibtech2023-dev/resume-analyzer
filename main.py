dc2547b8c7b8995fdaa8cb7d8367fe1db9c5d679
vaibhavibtech2023-dev <vaibhavi.btech2023@spsu.ac.in>
2026-04-23 16:02:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -191,5 +191,8 @@ def candidate():
 
 
 # ---------------- RUN APP ----------------
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    print("Starting app on port:", port)
+    app.run(host="0.0.0.0", port=port)
